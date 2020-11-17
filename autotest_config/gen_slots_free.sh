9d2be9495c4efd350ce914a1f0befb8841476c86
Simon Farrow <simon.farrow1@nhs.net>
2020-11-17 09:46:57 +0000

    Initial changes to extractor syntax

--- autotest_config/gen_slots_free.sh
+++ autotest_config/gen_slots_free.sh
@@ -4,8 +4,9 @@
 for f in {5..19}
 do
 	echo $f
+	# configure each extractor config file file with correct indices
 	sed -e s/__SLOT_NO__/s$f/ -e s/__POSITION__/$(($f-4))/ < slots_free_template.cfg > extractor_configs/slots_free$f.cfg
 
-	sed -i -e "/__ID__/s/$/	__APPT_ID__/" -e "/^s/s/$/	_/" \
-		extractor_configs/slot_$f.tdv
+	#  append a field title and an underscore field data item to the associated tdv
+	#sed -i -e "/__ID__/s/$/	__APPT_ID__/" -e "/^s/s/$/	_/" extractor_configs/slot_$f.tdv
 done
