7e89fd6c0d5fede5bc6d7e15739cc760f61fb81d
Richard Robinson <rrobinson@nhs.net>
2020-11-19 18:15:01 +0000

    fix refs in set-all-configurations script

--- set-all-configurations.sh
+++ set-all-configurations.sh
@@ -8,20 +8,20 @@ if [ -z "$1" ]
 fi
 
 # Update all instances of local dir with docker image directories for all config/contrib entries
-sed -i -e 's|TKW_ROOT/|/home/service/TKW/|g' ${configDirectory}/tkw-x_cleartext.properties
+sed -i -e 's|TKW_ROOT/|/home/service/TKW/|g' ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
 
 # Update all output directories with docker image volume directories
-sed -i -e "/^tks.evidencemetadata.location/c\tks.evidencemetadata.location /home/service/data/all_evidence" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.validator.reports/c\tks.validator.reports /home/service/data/all_evidence" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.logdir/c\tks.logdir /home/service/data/logs" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.savedmessages/c\tks.savedmessages /home/service/data/all_evidence" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.sender.destination/c\tks.sender.destination /home/service/data/transmitter_sent_messages" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.validator.source/c\tks.validator.source /home/service/data/messages_for_validation" ${configDirectory}/tkw-x_cleartext.properties
+sed -i -e "/^tks.evidencemetadata.location/c\tks.evidencemetadata.location /home/service/data/all_evidence" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.validator.reports/c\tks.validator.reports /home/service/data/all_evidence" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.logdir/c\tks.logdir /home/service/data/logs" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.savedmessages/c\tks.savedmessages /home/service/data/all_evidence" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.sender.destination/c\tks.sender.destination /home/service/data/transmitter_sent_messages" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.validator.source/c\tks.validator.source /home/service/data/messages_for_validation" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
 
 # Update FHIR profile directory with docker image volume directories (This should be done in a more programatic fasion with a loop - run out of time :-)) 
-sed -i -e "/^tks.validator.hapifhirvalidator.assetdir.ignore/c\tks.validator.hapifhirvalidator.assetdir.ignore /home/service/fhir/examples/" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.validator.hapifhirvalidator.assetdir /c\tks.validator.hapifhirvalidator.assetdir /home/service/fhir" ${configDirectory}/tkw-x_cleartext.properties
-sed -i -e "/^tks.validator.hapifhirvalidator.profileversionfilelocation/c\tks.validator.hapifhirvalidator.profileversionfilelocation /home/service/fhir/package.json" ${configDirectory}/tkw-x_cleartext.properties
+sed -i -e "/^tks.validator.hapifhirvalidator.assetdir.ignore/c\tks.validator.hapifhirvalidator.assetdir.ignore /home/service/fhir/examples/" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.validator.hapifhirvalidator.assetdir /c\tks.validator.hapifhirvalidator.assetdir /home/service/fhir" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
+sed -i -e "/^tks.validator.hapifhirvalidator.profileversionfilelocation/c\tks.validator.hapifhirvalidator.profileversionfilelocation /home/service/fhir/package.json" ${configDirectory}/tkw-x_cleartext_provider_simulator.properties
 
 # Update Simulator ruleset with docker image directories
 sed -i -e 's|TKW_ROOT/|/home/service/TKW/|g' ${configDirectory}/simulator_config/test_tks_rule_config.txt
