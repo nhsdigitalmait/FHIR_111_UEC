3df6b825fe7cdb4507a2c160feea8734a4ad13d5
Richard Robinson <rrobinson@nhs.net>
2020-11-19 17:47:51 +0000

    updated for use with docker

--- runtkwfhirvalidator.sh
+++ runtkwfhirvalidator.sh
@@ -13,8 +13,8 @@ if [ "$trustStore" == 'default' ]
 then
 	#ClearText
 	java -version
-	java -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_111_UEC/tkw-x_cleartext.properties
+	java -XX:+UseContainerSupport -XX:MaxRAMPercentage=80.0 -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_111_UEC/tkw-x_cleartext_provider_simulator.properties
 else
 	#TLSMA
-	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_111_UEC/tkw-x.properties
+	java -Djavax.net.ssl.trustStore=$trustStore -Djavax.net.ssl.trustStorePassword=$trustStorePassword -Djavax.net.ssl.keyStore=$keyStore -Djavax.net.ssl.keyStorePassword=$keyStorePassword -jar /home/service/TKW/TKW-x.jar -httpinterceptor /home/service/TKW/config/FHIR_111_UEC/tkw-x_cleartext_provider_simulator.properties
 fi
