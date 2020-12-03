d282e8153094efeabf7b96ad671539ec0ece1989
Richard Robinson <rrobinson@nhs.net>
2020-12-03 17:34:01 +0000

    Updates to the rulesets

--- build_provider_simulator_Docker.sh
+++ build_provider_simulator_Docker.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-DT=`date +%Y%m%d`
+#The argument is to label the container. If non is given then a date will be used
+label=$1
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied - date will be used for label"
+	$label=`date +%Y%m%d`
+fi
 #Update the docker ignore sim link
 ln -fs .dockerignore.simulator .dockerignore
 #Build the docker image
 docker build -f Dockerfile.simulator -t tkw_uec_provider_simulator .
 #Tag the docker image with today's date
-docker tag tkw_uec_provider_simulator nhsdigitalmait/tkw_uec_provider_simulator:$DT
+docker tag tkw_uec_provider_simulator nhsdigitalmait/tkw_uec_provider_simulator:$label
