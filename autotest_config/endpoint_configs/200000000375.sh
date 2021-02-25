40066f69e2c2c31f99042f2f91ec9d89ac1f3322
Richard Robinson <rrobinson@nhs.net>
2021-02-25 17:21:08 +0000

    updates to docker build scripts

--- autotest_config/endpoint_configs/200000000375.sh
+++ autotest_config/endpoint_configs/200000000375.sh
@@ -12,5 +12,5 @@ to_ep=https://rhm-ingleborough.cfh-nic.nhs.uk
 # via proxy
 #to_ep=https://proxy.int.spine2.ncrs.nhs.uk
 sendtls=Yes
-truststore=$HOME/Documents/SSP/whernside/SSP_whernside_20170719/certs/trust.jks
-keystore=$HOME/Documents/certs/INT_Certs/whernside_ssp/rhm-whernside.cfh-nic.nhs.uk.jks
+truststore=/home/service/Documents/SSP/whernside/SSP_whernside_20170719/certs/trust.jks
+keystore=/home/service/Documents/certs/INT_Certs/whernside_ssp/rhm-whernside.cfh-nic.nhs.uk.jks
---
+++ build_provider_simulator_Docker.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# build docker image for emergency booking consumer simulator
+# usage build_provider_simulator_Docker.sh [<userid>]
+# if no user id is provided it defaults to 1000 and the tag is just the version number
+#
+TAG=0.4
+
+if [[ "$1" == "" ]]
+then
+	USER_ID=1000
+else
+	USER_ID=$1
+	TAG+=-$USER_ID
+fi
+
+IMAGENAME=tkw_uec_provider_simulator
+PROJECT=FHIR_111_UEC
+
+echo "Building $IMAGENAME:$TAG"
+read -n 1 -p "Press any key to continue..."
+echo building
+
+# the source folder must be in install mode (with real paths) not uninistall mode (TKW_ROOT)
+cd $TKWROOT/config/$PROJECT
+fixtkwroot.sh .
+cd -
+
+# put the git commit hash and date into a text file
+echo "111 UEC Booking Provider Simulator Version: $TAG"  > version_string.txt
+echo "111 UEC Booking Github repository shortcode:" `git show -s --format="$PROJECT %h %cI"` >> version_string.txt
+
+
+#Update the docker ignore sim link
+ln -fs .dockerignore.provider.simulator .dockerignore
+#Build the docker image
+docker build --build-arg USER_ID=$USER_ID -f Dockerfile.provider.simulator -t nhsdigitalmait/$IMAGENAME:$TAG .
+echo Docker Image tagged with $TAG
